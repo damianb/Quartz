5af6bf4b3139fcb4ec749d2637008721f25c0965
Obsidian <stratosphere.programming@gmail.com>
2011-03-14 14:34:41 -0500

    Toss in two default exceptions, we'll use them in the page processor

---
+++ Exception/ServerErrorException.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Quartz
+ *-------------------------------------------------------------------
+ * @category    Quartz
+ * @package     Quartz
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/Quartz
+ *
+ *===================================================================
+ *
+ */
+
+namespace Codebite\Quartz\Exception;
+
+/**
+ * Quartz - Server Error Exception class,
+ * 		Thrown when we want to say that something server-side went boom.
+ *
+ *
+ * @category    Quartz
+ * @package     Quartz
+ * @author      Damian Bushong
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/damianb/Quartz
+ */
+class ServerErrorException extends \Exception { }
