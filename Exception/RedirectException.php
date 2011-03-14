bc546a7dd1c5bd0c3ab78a7d94d6d10a7975b868
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-03-14 14:34:41 -0500

    Toss in two default exceptions, we'll use them in the page processor

---
+++ Exception/RedirectException.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Quartz
+ *-------------------------------------------------------------------
+ * @category    Quartz
+ * @package     Quartz
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/Quartz
+ *
+ *===================================================================
+ *
+ */
+
+namespace Codebite\Quartz\Exception;
+
+/**
+ * Quartz - Redirect Exception class,
+ * 		Used to trigger an HTTP redirect of a user to another page.
+ *
+ *
+ * @category    Quartz
+ * @package     Quartz
+ * @author      Damian Bushong
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/damianb/Quartz
+ */
+class RedirectException extends \Exception { }
