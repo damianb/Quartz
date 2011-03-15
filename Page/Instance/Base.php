c953cea2d5608ddcb9bdd32dc9092331916b1ef1
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-03-14 20:13:30 -0500

    Fill out the page instance base.

---
+++ Page/Instance/Base.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Quartz
+ *-------------------------------------------------------------------
+ * @category    Quartz
+ * @package     Quartz
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/Quartz
+ *
+ *===================================================================
+ *
+ */
+
+namespace Codebite\Quartz\Page\Instance;
+use \OpenFlame\Framework\Core;
+
+/**
+ * Quartz - Base page class,
+ * 		Provides a base for page instances to extend.
+ *
+ *
+ * @category    Quartz
+ * @package     Quartz
+ * @author      Damian Bushong
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/damianb/Quartz
+ */
+abstract class Base
+{
+	protected $route;
+
+	protected $template_name = '';
+
+	public function setRoute(\OpenFlame\Framework\URL\RouteInstance $route)
+	{
+		$this->route = $route;
+		return $this;
+	}
+
+	public static function newInstance()
+	{
+		return Core::setObject('page.instance', new static());
+	}
+
+	public static function newRoutedInstance(\OpenFlame\Framework\URL\RouteInstance $route)
+	{
+		$self = static::newInstance();
+		$self->setRoute($route)
+			->executePage();
+
+		return $self;
+	}
+
+	public function getTemplateName()
+	{
+		return $this->template_name;
+	}
+
+	abstract public function executePage();
+}
