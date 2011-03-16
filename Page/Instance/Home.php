ed6af0c0214c9c8d6ecfb55823b236f6ba6722b0
Obsidian <stratosphere.programming@gmail.com>
2011-03-16 14:03:21 -0500

    Add in the error and home pages

---
+++ Page/Instance/Home.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Quartz
+ *-------------------------------------------------------------------
+ * @category    Quartz
+ * @package     Quartz
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/Quartz
+ *
+ *===================================================================
+ *
+ */
+
+namespace Codebite\Quartz\Page\Instance;
+use \OpenFlame\Framework\Core;
+
+/**
+ * Quartz - Default page object,
+ * 		The home page.
+ *
+ *
+ * @category    Quartz
+ * @package     Quartz
+ * @author      Damian Bushong
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/damianb/Quartz
+ */
+class Home extends \Codebite\Quartz\Page\Instance\Base
+{
+	protected $template_name = 'home.twig.html';
+}
