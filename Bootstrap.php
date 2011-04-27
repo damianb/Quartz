77f6c972318e71fa9e9dc02253e93b9f015fbe8e
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-04-27 13:29:01 -0500

    Cleanup of the bootstrap file, to match changes in the OpenFlame Framework

--- Bootstrap.php
+++ Bootstrap.php
@@ -19,6 +19,7 @@
 namespace Codebite\Quartz;
 use \OpenFlame\Framework\Core;
 use OpenFlame\Framework\Event\Instance as Event;
+use OpenFlame\Framework\Utility\JSON;
 
 /**
  * @ignore
@@ -29,7 +30,7 @@ if(!defined('Codebite\\Quartz\\SITE_ROOT')) exit;
 set_exception_handler('\\Codebite\\Quartz\\Exception\\Handler::catcher');
 
 // Load the config file and its data.
-$config_data = \OpenFlame\Framework\Utility\JSON::decode(\Codebite\Quartz\SITE_ROOT . '/data/config/config.json');
+$config_data = JSON::decode(\Codebite\Quartz\SITE_ROOT . '/data/config/config.json');
 foreach($config_data as $config_name => $config_value)
 {
 	Core::setConfig($config_name, $config_value);
@@ -39,15 +40,15 @@ foreach($config_data as $config_name => $config_value)
  * Load up the core objects
  */
 $timer = Core::setObject('timer', new \OpenFlame\Framework\Utility\Timer());
-$router = Core::setObject('router', new \OpenFlame\Framework\URL\Router());
+$router = Core::setObject('router', new \OpenFlame\Framework\Router\Router());
 $input = Core::setObject('input', new \OpenFlame\Framework\Input\Handler());
-$template = Core::setObject('template', new \OpenFlame\Framework\Template\Variables());
-$asset_manager = Core::setObject('asset_manager', new \OpenFlame\Framework\Template\Asset\Manager());
+$template = Core::setObject('template', new \OpenFlame\Framework\Twig\Variables());
+$asset_manager = Core::setObject('asset_manager', new \OpenFlame\Framework\Asset\Manager());
 $dispatcher = Core::setObject('dispatcher', new \OpenFlame\Framework\Event\Dispatcher());
 $processor = Core::setObject('processor', new \Codebite\Quartz\Page\Processor());
 $language_handler = Core::setObject('language', new \OpenFlame\Framework\Language\Handler());
 $header_manager = Core::setObject('header', new \OpenFlame\Framework\Header\Manager());
-$twig = Core::setObject('twig.frontend', new \OpenFlame\Framework\Template\Twig());
+$twig = Core::setObject('twig.frontend', new \OpenFlame\Framework\Twig\Wrapper());
 
 // Set the base URL for HTTP stuff.
 $base_url = Core::getConfig('page.base_url') ?: '/';
@@ -59,77 +60,77 @@ $asset_manager->setBaseURL($base_url);
  */
 
 // Start up the cache subsystem.
-$dispatcher->register('cache.load', function(Event $event) {
+$dispatcher->register('cache.load', 0, function(Event $event) {
 	$cache_engine = new \OpenFlame\Framework\Cache\Engine\EngineJSON();
 	$cache_engine->setCachePath(\Codebite\Quartz\SITE_ROOT . '/cache/');
 	$cache = Core::setObject('cache', new \OpenFlame\Framework\Cache\Driver());
 	$cache->setEngine($cache_engine);
-}, array(), 0);
+});
 
 // Set twig properties
-$dispatcher->register('twig.load', function(Event $event) use($twig) {
+$dispatcher->register('twig.load', 0, function(Event $event) use($twig) {
 	$twig->setTwigRootPath(\OpenFlame\ROOT_PATH . '/vendor/Twig/lib/Twig/')
 		->setTwigCachePath(\Codebite\Quartz\SITE_ROOT . '/cache/twig/')
 		->setTwigOption('autoescape', false)
 		->setTwigOption('debug', true)
 		->setTemplatePath(\Codebite\Quartz\SITE_ROOT . '/data/template/');
-}, array(), 0);
+});
 
 // Load twig
-$dispatcher->register('twig.load', function(Event $event) use($twig) {
+$dispatcher->register('twig.load', 10, function(Event $event) use($twig) {
 	$twig->initTwig();
-}, array(), 10);
+});
 
 // Snag control of the headers
-$dispatcher->register('page.headers.snag', function(Event $event) use($header_manager) {
+$dispatcher->register('page.headers.snag', 0, function(Event $event) use($header_manager) {
 	$header_manager->snagHeaders();
-}, array(), 0);
+});
 
 // Define our assets...
-$dispatcher->register('page.assets.define', function(Event $event) use($asset_manager) {
+$dispatcher->register('page.assets.define', 0, function(Event $event) use($asset_manager) {
 	$asset_manager->registerJSAsset('jquery')->setURL('/style/js/jquery.min.js');
 	$asset_manager->registerCSSAsset('common')->setURL('/style/css/common.css');
-}, array(), 0);
+});
 
 // Create the template proxies and load them into twig
-$dispatcher->register('page.assets.define', function(Event $event) use($twig, $timer, $asset_manager, $language_handler) {
+$dispatcher->register('page.assets.define', 19, function(Event $event) use($twig, $timer, $asset_manager, $language_handler) {
 	$twig_env = $twig->getTwigEnvironment();
 	$twig_env->addGlobal('timer', $timer);
-	$twig_env->addGlobal('asset', new \OpenFlame\Framework\Template\Asset\Proxy($asset_manager));
+	$twig_env->addGlobal('asset', new \OpenFlame\Framework\Asset\Proxy($asset_manager));
 	$twig_env->addGlobal('language', new \OpenFlame\Framework\Language\Proxy($language_handler));
-}, array(), 19);
+});
 
 // Enable invalid asset exceptions (lowest priority listener!)
-$dispatcher->register('page.assets.define', function(Event $event) use($asset_manager) {
+$dispatcher->register('page.assets.define', 20, function(Event $event) use($asset_manager) {
 	$asset_manager->enableInvalidAssetExceptions();
-}, array(), 20);
+});
 
 // Load our routes.
-$dispatcher->register('page.routes.load', function(Event $event) use($processor) {
+$dispatcher->register('page.routes.load', 15, function(Event $event) use($processor) {
 	$processor->loadRoutes();
-}, array(), 15);
+});
 
 // Load the language file
-$dispatcher->register('page.language.load', function(Event $event) use($language_handler) {
-	$language_entries = \OpenFlame\Framework\Utility\JSON::decode(\Codebite\Quartz\SITE_ROOT . '/data/language/en.json');
+$dispatcher->register('page.language.load', 15, function(Event $event) use($language_handler) {
+	$language_entries = JSON::decode(\Codebite\Quartz\SITE_ROOT . '/data/language/en.json');
 	$language_handler->loadEntries($language_entries);
-}, array(), 15);
+});
 
 // Send headers
-$dispatcher->register('page.headers.send', function(Event $event) use($header_manager) {
+$dispatcher->register('page.headers.send', 10, function(Event $event) use($header_manager) {
 	$header_manager->sendHeaders();
-}, array(), 10);
+});
 
 // Execute the page logic
-$dispatcher->register('page.execute', function(Event $event) use($dispatcher, $processor) {
+$dispatcher->register('page.execute', 10, function(Event $event) use($dispatcher, $processor) {
 	$page = $processor->run();
 	$page->executePage();
 
 	$dispatcher->triggerUntilBreak(Event::newEvent('page.display')->setDataPoint('page', $page));
-}, array(), 10);
+});
 
 // Display the page
-$dispatcher->register('page.display', function(Event $event) use($dispatcher, $twig, $template) {
+$dispatcher->register('page.display', 10, function(Event $event) use($dispatcher, $twig, $template) {
 	$page = $event->getDataPoint('page');
 	$twig_env = $twig->getTwigEnvironment();
 	$twig_page = $twig_env->loadTemplate($page->getTemplateName());
@@ -146,4 +147,4 @@ $dispatcher->register('page.display', function(Event $event) use($dispatcher, $t
 		ob_clean();
 		throw $e;
 	}
-}, array(), 10);
+});
