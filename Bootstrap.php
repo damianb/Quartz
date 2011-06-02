55df7415ae96d8d388c4ab5c3956e7da3a2ff098
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-06-02 13:02:35 -0500

    More cleanup, and let's use some self-referencing dep injection.
    Also adding some configs to use later on down the road if needed (I bet sam would like them)

--- Bootstrap.php
+++ Bootstrap.php
@@ -17,9 +17,10 @@
  */
 
 namespace Codebite\Quartz;
-use \OpenFlame\Framework\Core;
+use OpenFlame\Framework\Core;
 use OpenFlame\Framework\Event\Instance as Event;
 use OpenFlame\Framework\Utility\JSON;
+use OpenFlame\Framework\Dependency\Injector;
 
 /**
  * @ignore
@@ -76,7 +77,7 @@ $base_url = Core::getConfig('page.base_url') ?: '/';
  * recommended commit: fae7e2f315c8699cf1367035682c3068899f8c92
  */
 $timer = Core::setObject('timer', new \OpenFlame\Framework\Utility\Timer());
-$injector = \OpenFlame\Framework\Dependency\Injector::getInjector();
+$injector = Injector::getInjector();
 
 $injector->setInjector('router', function() use($base_url) {
     $router = new \OpenFlame\Framework\Router\Router();
@@ -92,10 +93,14 @@ $injector->setInjector('template', function() {
     return new \OpenFlame\Framework\Twig\Variables();
 });
 
-$injector->setInjector('asset_manager', function() use($base_url) {
+$injector->setInjector('asset', function() use($base_url) {
     $asset_manager = new \OpenFlame\Framework\Asset\Manager();
     $asset_manager->setBaseURL($base_url);
     return $asset_manager;
+});
+
+$injector->setInjector('asset_proxy', function() use($injector) {
+    return new \OpenFlame\Framework\Asset\Proxy($injector->get('asset'));
 });
 
 $injector->setInjector('dispatcher', function() {
@@ -104,6 +109,10 @@ $injector->setInjector('dispatcher', function() {
 
 $injector->setInjector('processor', function() {
     return new \Codebite\Quartz\Page\Processor();
+});
+
+$injector->setInjector('language_proxy', function() use($injector) {
+    return new \OpenFlame\Framework\Language\Proxy($injector->get('language'));
 });
 
 $injector->setInjector('language', function() {
@@ -128,19 +137,23 @@ $injector->setInjector('seeder', function() {
 $injector->setInjector('twig', function() {
     $twig = new \OpenFlame\Framework\Twig\Wrapper();
     $twig->setTwigRootPath(\OpenFlame\ROOT_PATH . '/vendor/Twig/lib/Twig/')
-    	->setTwigCachePath(\Codebite\Quartz\SITE_ROOT . '/cache/twig/')
-		->setTemplatePath(\Codebite\Quartz\SITE_ROOT . '/data/template/')
-    	->setTwigOption('debug', true);
+		->setTwigCachePath((Core::getConfig('twig.cache_path') ?: \Codebite\Quartz\SITE_ROOT . '/cache/twig/'))
+		->setTemplatePath((Core::getConfig('twig.template_path') ?: \Codebite\Quartz\SITE_ROOT . '/data/template/'))
+		->setTwigOption('debug', (Core::getConfig('twig.debug') ?: true));
     $twig->initTwig();
 
     return $twig;
 });
 
-$injector->setInjector('cache', function() {
-    $cache_engine = new \OpenFlame\Framework\Cache\Engine\File\FileEngineJSON();
+$injector->setInjector('cache_engine', function() {
+	$cache_engine = new \OpenFlame\Framework\Cache\Engine\File\FileEngineJSON();
     $cache_engine->setCachePath(\Codebite\Quartz\SITE_ROOT . '/cache/');
+	return $cache_engine;
+});
+
+$injector->setInjector('cache', function() use($injector) {
 	$cache = new \OpenFlame\Framework\Cache\Driver();
-	$cache->setEngine($cache_engine);
+	$cache->setEngine($injector->get('cache_engine'));
     return $cache;
 });
 
@@ -152,61 +165,72 @@ $dispatcher = $injector->get('dispatcher');
 
 
 // Create the template proxies and load them into twig
-$dispatcher->register('page.assets.define', 19, function(Event $event) use($injector) {
+$dispatcher->register('page.assets.define', 18, function(Event $event) use($injector) {
     $twig = $injector->get('twig');
-    $timer = $injector->get('timer');
-    $asset_manager = $injector->get('asset_manager');
-    $language = $injector->get('language');
+
     $twig_env = $twig->getTwigEnvironment();
-	$twig_env->addGlobal('timer', $timer);
-	$twig_env->addGlobal('asset', new \OpenFlame\Framework\Asset\Proxy($asset_manager));
-	$twig_env->addGlobal('language', new \OpenFlame\Framework\Language\Proxy($language_handler));
-});
-
-
-// Snag control of the headers
-$dispatcher->register('page.headers.snag', 0, function(Event $event) use($header_manager) {
-	$header_manager->snagHeaders();
+	$twig_env->addGlobal('timer', $injector->get('timer'));
+	$twig_env->addGlobal('asset', $injector->get('asset_proxy'));
+	$twig_env->addGlobal('language', $injector->get('language_proxy'));
 });
 
 // Define our assets...
-$dispatcher->register('page.assets.define', 0, function(Event $event) use($asset_manager) {
+$dispatcher->register('page.assets.define', 0, function(Event $event) use($injector) {
+	$asset_manager = $injector->get('asset');
 	$asset_manager->registerJSAsset('jquery')->setURL('/style/js/jquery.min.js');
 	$asset_manager->registerCSSAsset('common')->setURL('/style/css/common.css');
 });
 
 // Enable invalid asset exceptions (lowest priority listener!)
-$dispatcher->register('page.assets.define', 20, function(Event $event) use($asset_manager) {
+$dispatcher->register('page.assets.define', 19, function(Event $event) use($injector) {
+	$asset_manager = $injector->get('asset');
 	$asset_manager->enableInvalidAssetExceptions();
 });
 
 // Load our routes.
-$dispatcher->register('page.routes.load', 15, function(Event $event) use($processor) {
+$dispatcher->register('page.routes.load', 15, function(Event $event) use($injector) {
+	$processor = $injector->get('processor');
 	$processor->loadRoutes();
 });
 
 // Load the language file
-$dispatcher->register('page.language.load', 15, function(Event $event) use($language_handler) {
-	$language_entries = JSON::decode(\Codebite\Quartz\SITE_ROOT . '/data/language/en.json');
-	$language_handler->loadEntries($language_entries);
+$dispatcher->register('page.language.load', 15, function(Event $event) use($injector) {
+	$language = $injector->get('language');
+	$language_entries = JSON::decode((Core::getConfig('language.file') ?: \Codebite\Quartz\SITE_ROOT . '/data/language/en.json'));
+	$language->loadEntries($language_entries);
+});
+
+// Snag control of the headers
+$dispatcher->register('page.headers.snag', 0, function(Event $event) use($injector) {
+	$header_manager = $injector->get('header');
+	$header_manager->snagHeaders();
 });
 
 // Send headers
-$dispatcher->register('page.headers.send', 10, function(Event $event) use($header_manager) {
+$dispatcher->register('page.headers.send', 10, function(Event $event) use($injector) {
+	$header_manager = $injector->get('header');
 	$header_manager->sendHeaders();
 });
 
 // Execute the page logic
-$dispatcher->register('page.execute', 10, function(Event $event) use($dispatcher, $processor) {
+$dispatcher->register('page.execute', 10, function(Event $event) use($injector) {
+	$processor = $injector->get('processor');
+	$dispatcher = $injector->get('dispatcher');
+
 	$page = $processor->run();
 	$page->executePage();
 
-	$dispatcher->triggerUntilBreak(Event::newEvent('page.display')->setDataPoint('page', $page));
+	Core::setObject('page', $page);
+	$dispatcher->triggerUntilBreak(Event::newEvent('page.display'));
 });
 
 // Display the page
-$dispatcher->register('page.display', 10, function(Event $event) use($dispatcher, $twig, $template) {
-	$page = $event->getDataPoint('page');
+$dispatcher->register('page.display', 10, function(Event $event) use($injector) {
+	$page = Core::getObject('page');
+	$twig = $injector->get('twig');
+	$dispatcher = $injector->get('dispatcher');
+	$template = $injector->get('template');
+
 	$twig_env = $twig->getTwigEnvironment();
 	$twig_page = $twig_env->loadTemplate($page->getTemplateName());
 	try
