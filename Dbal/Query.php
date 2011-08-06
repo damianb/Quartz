c26cb378b883552fb0c0addd01866ab9c984dd65
Obsidian <stratosphere.programming@gmail.com>
2011-08-06 15:25:46 -0500

    Add in this empty class

---
+++ Dbal/Query.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  Quartz
+ *-------------------------------------------------------------------
+ * @category    Quartz
+ * @package     Quartz
+ * @author      Damian Bushong
+ * @copyright   (c) 2009 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/damianb/Quartz
+ *
+ *===================================================================
+ *
+ */
+
+namespace Codebite\Quartz\Dbal;
+use \Codebite\Quartz\Site as Quartz;
+
+class Query extends \OpenFlame\Dbal\Query
+{
+	// asdf
+}
