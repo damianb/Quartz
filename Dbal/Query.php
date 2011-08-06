ff506c60edd6df2d216052056843bec9310020dd
Obsidian <stratosphere.programming@gmail.com>
2011-08-06 15:36:08 -0500

    give some beef to query

--- Dbal/Query.php
+++ Dbal/Query.php
@@ -21,5 +21,33 @@ use \Codebite\Quartz\Site as Quartz;
 
 class Query extends \OpenFlame\Dbal\Query
 {
-	// asdfghjkl
+	/**
+	 * Chains onto the original method, tacks on some timing mechanisms
+	 */
+	public function _query($hard = false)
+	{
+		$quartz = Quartz::getInstance();
+
+		$instance = NULL;
+		// Fire the debug timing tick
+		$quartz->debugtime->newEntry('query->query', 'Debug timing tick fired before Query->_query() execution', $instance,
+			array(
+				'sql'			=> $this->sql,
+			)
+		);
+
+		parent::_query($hard);
+
+		// Fire the debug timing tick
+		$quartz->debugtime->newEntry('query->query', 'Debug timing tick fired after Query->_query() execution', $instance);
+	}
+
+	/**
+	 * @ignore
+	 * Debugging backdoor to grab built SQL after build() has been called
+	 */
+	public function _getSQL()
+	{
+		return $this->sql;
+	}
 }
